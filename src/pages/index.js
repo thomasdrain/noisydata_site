60afadbad91eb5a6a7d4c837cf8b80ef01a32880
unknown <t.drain@hotmail.com>
2020-06-12 22:56:23 +0100

    Joke title

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    const siteTitle = "noisydata - a thomas drain production"
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
